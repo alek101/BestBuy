beff6d2846381149b62d78f7bd319259010b69b7
alek101 <47416711+alek101@users.noreply.github.com>
2020-09-11 22:37:35 +0200

    Initial commit
    I created the simplest possible database and added reletions.

---
+++ database/migrations/2020_09_11_194145_create_products_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->char('model',50);
+            $table->char('type',50);
+            $table->unsignedBigInteger('category');
+            $table->foreign('category')->references('id')->on('categories')->onDelete('cascade');
+            $table->char('manufacturor',50);
+            $table->integer('serial');
+            $table->integer('sku');
+            $table->decimal('prise',10,2);
+            $table->decimal('discount',10,2);
+            $table->mediumText('description');
+            $table->mediumText('link');
+
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
