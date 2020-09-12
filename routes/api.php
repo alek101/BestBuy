bf0fc412c3f0942db2f8654729c5d32b10bce232
alek101 <47416711+alek101@users.noreply.github.com>
2020-09-12 11:26:48 +0200

    Update api.php
    Added routes to api routes as well.

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\FileController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\ProductController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +17,23 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::post('/uploadfile',[FileController::class, 'addCSV']);
+
+Route::prefix('/categories')->group(function()
+{
+    Route::get('/index',[CategoryController::class, 'index']);
+    Route::post('/update',[CategoryController::class, 'update']);
+    Route::get('/delete/{id}',[CategoryController::class, 'destroy']);
+});
+
+Route::prefix('/products')->group(function()
+{
+    Route::get('/index',[ProductController::class, 'index']);
+    Route::get('/showProductsByCategoryID/{id}',[ProductController::class, 'showProductsByCategoryID']);
+    Route::post('/update',[ProductController::class, 'update']);
+    Route::get('/delete/{id}',[ProductController::class, 'destroy']);
 });
